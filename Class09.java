a9b671756ace80c66b116bf5032d049c1a14ea74
songwun <92703380+songwun@users.noreply.github.com>
2021-11-14 16:39:47 +0800

    Add files via upload

---
+++ Class09.java
@@ -0,0 +1,23 @@
+package Java10;
+class Caaa {
+	private int num;
+
+	public void display() {
+		System.out.println("printed from Caaa class");
+	}
+}
+
+class Cbbb extends Caaa {
+	public void display() {
+		System.out.println("printed from Cbbb class");
+	}
+}
+
+public class Class09 {
+	public static void main(String[] args) {
+		Caaa bb=new Cbbb(); 
+		bb.display(); 
+	}
+
+}
+
